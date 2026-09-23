fdc5698baf3ea4c8ea642326fcbbddb7fcfdc4cb
MoolaShashank <122132928+MoolaShashank@users.noreply.github.com>
2026-09-23 09:18:02 +0530

    Add files via upload

---
+++ apps/core/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+
+app_name = "core"
+urlpatterns = [path("", views.home, name="home"), path("about/", views.about, name="about"), path("privacy/", views.privacy, name="privacy")]
